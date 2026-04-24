07c10838b141e1f9aac4c26f822f80538a6ffb5e
Tobias Vera <tvera@dlstechnology.com.py>
2026-04-24 16:25:45 -0300

    part2-c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ContactForm from './components/ContactForm'
 import Filter from './components/Filter'
 import Contacts from './components/Contacts'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterBy, setFilterBy] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -44,9 +48,9 @@ const App = () => {
     setFilterBy(event.target.value)
   }
 
-  const filteredList = filterBy ? 
-      persons.filter(person => person.name.toLowerCase().includes(filterBy.toLowerCase())) : 
-      persons
+  const filteredList = filterBy 
+      ? persons.filter(person => person.name.toLowerCase().includes(filterBy.toLowerCase())) 
+      : persons
 
   return (
     <div>
